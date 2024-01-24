d31d6d09bc1ac2e3b981cd9a9cb0d2946b407022
Jamez Mayke <jamezmayke@gmail.com>
2024-01-24 10:02:30 -0300

    criando diretórios model e servicce e já criando o consumo dos filmes em JSON

--- apitopmoviesimdb/src/br/com/topmoviesimdb/model/Start.java
+++ apitopmoviesimdb/src/br/com/topmoviesimdb/model/Start.java
@@ -1,7 +1,21 @@
 package br.com.topmoviesimdb.model;
 
+import br.com.topmoviesimdb.service.ConsumoApi;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class Start {
     public static void main(String[] args ) {
-        System.out.println("Hello, new project!");
+        String endereco = "https://mocki.io/v1/9a7c1ca9-29b4-4eb3-8306-1adb9d159060";
+
+        ConsumoApi consumo = new ConsumoApi();
+
+        String resultado = consumo.retornaLista(endereco);
+        System.out.println(resultado);
+
     }
 }
---
+++ apitopmoviesimdb/src/br/com/topmoviesimdb/service/ConsumoApi.java
@@ -0,0 +1,25 @@
+package br.com.topmoviesimdb.service;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ConsumoApi {
+    public String retornaLista(String endereco) {
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest
+                    .newBuilder()
+                    .uri(URI.create(endereco))
+                    .build();
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+
+            return response.body();
+        } catch (IOException | InterruptedException e) {
+            return "Não foi possível chamar a API";
+        }
+    }
+}
